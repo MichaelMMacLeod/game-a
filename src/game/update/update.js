716942fb079ce92a776208a89070ee3a3fcf2680
Michael MacLeod <michaelmmacleod@gmail.com>
2018-02-10 18:18:04 -0800

    Re-add vector movement
    I messed up fixing merge conflicts :/

--- src/game/update/update.js
+++ src/game/update/update.js
@@ -1,11 +1,17 @@
 var mod = {
     controller: require('../controller/controller.js'),
-    camera: require('../camera/camera.js')
+    camera: require('../camera/camera.js'),
+    part: require('../part/part.js')
 };
 
 var create = (state) => {
     return () => {
         mod.controller.process(state.controller, state);
+
+        mod.part.translate(
+            state.parts[0],
+            state.parts[0].ax,
+            state.parts[0].ay);
 
         var blocks = state.parts.reduce((acc, v) => {
             acc.push(v.block);
